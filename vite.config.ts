1e692701195267dadfddccda5693b75ea4141850
Dipan Samanta <samantadipan28@gmail.com>
2025-12-16 12:45:30 +0530

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "./",              // 👈 change this
+  base: "/For-you/",            // 👈 change this
   server: { host: "::", port: 8080 },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
